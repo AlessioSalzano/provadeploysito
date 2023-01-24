d046a4a4f0ce49503622205e43efaf7a0075d99c
AlessioSalzano <81223321+AlessioSalzano@users.noreply.github.com>
2023-01-24 12:01:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from "react"
+import axios from "axios"
 
 function App() {
+
+  const[users,setUsers] = useState([]);
+
+
+  useEffect (()=>{
+    richiesta()
+    
+    console.log(JSON.stringify(users))
+  },[])
+  
+  const richiesta =async ()=>{
+    const res= await axios.get("http://localhost:5000/utenti");
+    console.log(JSON.stringify(res.data))
+    let ARR=[]
+
+    let arr={"result":""}
+    arr.result=res.data
+    console.log(JSON.stringify(arr))
+    setUsers(JSON.stringify(arr))
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CIAO MONDO</h1>
     </div>
   );
 }
